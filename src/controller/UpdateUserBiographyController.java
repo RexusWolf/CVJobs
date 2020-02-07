175b5926c8ca9833e598eacad7ee861147cfaaaf
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2020-02-07 20:05:40 +0100

    Update bio controller done

---
+++ src/controller/UpdateUserBiographyController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import database.SkillDao;
+import database.UserDao;
+import model.User;
+
+@WebServlet("/updateBiography")
+public class UpdateUserBiographyController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	private UserDao userDao;
+    
+    public void init() {
+        userDao = new UserDao();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String email = request.getParameter("email");
+        String biography = request.getParameter("biography");
+       
+        try {
+        	userDao.updateUserBiography(email, biography);
+        } catch (Exception exception) {
+			exception.printStackTrace();
+        }
+        
+		response.sendRedirect("myProfile");
+	}
+}
